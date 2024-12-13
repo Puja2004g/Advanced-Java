744f2aac39e0f4958f54a81fb5fa38402cc13ee6
Puja2004g <pujaguchhait2004@gmail.com>
2024-12-13 19:45:58 +0530

    first commit

--- enumDatatype.java
+++ enumDatatype.java
@@ -1,4 +1,20 @@
 public class enumDatatype {
+    enum day{
+        sunday(1), monday(2), tuesday(3), wednesday(4), thursday(5), friday(6), saturday(7);
 
-    
+        private final int value;
+
+        day(int val){
+            this.value = val;
+        }
+
+        int getvalue(){
+            return value;
+        }
+    }
+    public static void main(String[] args) {
+        for(day d1 : day.values()){
+            System.out.println(d1 + "->" + d1.value);
+        }
+    }
 }
